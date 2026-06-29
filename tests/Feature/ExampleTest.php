37081a3c8d5caf5d760952d7eafe73dec50ffdbc
masud <mrmasud151821@gmail.com>
2026-06-29 16:53:34 +0600

    Modified

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    use RefreshDatabase;
+
+    public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
 
